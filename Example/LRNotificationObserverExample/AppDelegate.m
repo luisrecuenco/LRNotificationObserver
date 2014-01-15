929eb01ccf6078400104bc16a0989cc1c3624483
Luis Recuenco <luisrecuenco@gmail.com>
2014-01-15 01:22:21 +0100

    Created LRNotificationObserver category with NSNotificationObserver dependency already injected for convenience

--- Example/LRNotificationObserverExample/AppDelegate.m
+++ Example/LRNotificationObserverExample/AppDelegate.m
@@ -1,13 +1,28 @@
+// AppDelegate.m
 //
-//  AppDelegate.m
-//  LRNotificationObserverExample
+// Copyright (c) 2013 Luis Recuenco
 //
-//  Created by Luis Recuenco on 30/10/13.
-//  Copyright (c) 2013 Luis Recuenco. All rights reserved.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #import "AppDelegate.h"
 #import "LRNotificationObserver.h"
+#import "LRNotificationObserver+NSNotificationCenter.h"
 
 @interface AppDelegate ()
 
@@ -21,31 +36,25 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     
-    NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
+    __weak typeof(self) wself = self;
+    self.backgroundObserver = [LRNotificationObserver observerForName:UIApplicationDidEnterBackgroundNotification
+                                                                block:^(NSNotification *note) {
+                                                                    __strong typeof(wself) sself = wself;
+                                                                    [sself showLocalNotification];
+                                                                }];
     
-    self.backgroundObserver = [[LRNotificationObserver alloc] initWithNotificationCenter:notificationCenter];
-    
-    [self.backgroundObserver configureForName:UIApplicationDidEnterBackgroundNotification
-                                        block:^(NSNotification *note) {
-                                            UILocalNotification *notification = [[UILocalNotification alloc] init];
-                                            notification.alertBody = @"Background observer fired";
-                                            notification.fireDate = [NSDate date];
-                                            [application presentLocalNotificationNow:notification];
-                                        }];
-    
-    self.foregroundObserver = [[LRNotificationObserver alloc] initWithNotificationCenter:notificationCenter];
-    
-    [self.foregroundObserver configureForName:UIApplicationWillEnterForegroundNotification
-                               operationQueue:[NSOperationQueue mainQueue]
-                                       target:self
-                                       action:@selector(foregroundObserverFired:)];
+    self.foregroundObserver = [LRNotificationObserver observerForName:UIApplicationWillEnterForegroundNotification
+                                                       operationQueue:[NSOperationQueue mainQueue]
+                                                               target:self
+                                                               action:@selector(foregroundObserverFired:)];
     
     return YES;
 }
+
+#pragma mark - LRNotificationObserver actions
 
 - (void)foregroundObserverFired:(NSNotification *)notification
 {
@@ -55,6 +64,14 @@
                                           cancelButtonTitle:@"ok"
                                           otherButtonTitles:nil];
     [alert show];
+}
+
+- (void)showLocalNotification
+{
+    UILocalNotification *notification = [[UILocalNotification alloc] init];
+    notification.alertBody = @"Background observer fired";
+    notification.fireDate = [NSDate date];
+    [[UIApplication sharedApplication] presentLocalNotificationNow:notification];
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
---
+++ LRNotificationObserver/LRNotificationObserver+NSNotificationCenter.m
@@ -0,0 +1,98 @@
+// LRNotificationObserver+NSNotificationCenter.m
+//
+// Copyright (c) 2014 Luis Recuenco
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "LRNotificationObserver+NSNotificationCenter.h"
+
+@implementation LRNotificationObserver (NSNotificationCenter)
+
++ (instancetype)observerForName:(NSString *)name
+                          block:(LRNotificationObserverBlock)block
+{
+    return [self observerWithConfiguration:^(LRNotificationObserver *observer) {
+        [observer configureForName:name block:block];
+    }];
+}
+
++ (instancetype)observerForName:(NSString *)name
+                 operationQueue:(NSOperationQueue *)operationQueue
+                          block:(LRNotificationObserverBlock)block
+{
+    return [self observerWithConfiguration:^(LRNotificationObserver *observer) {
+        [observer configureForName:name operationQueue:operationQueue block:block];
+    }];
+}
+
++ (instancetype)observerForName:(NSString *)name
+                  dispatchQueue:(dispatch_queue_t)dispatchQueue
+                          block:(LRNotificationObserverBlock)block
+{
+    return [self observerWithConfiguration:^(LRNotificationObserver *observer) {
+        [observer configureForName:name dispatchQueue:dispatchQueue block:block];
+    }];
+}
+
++ (instancetype)observerForName:(NSString *)name
+                         target:(id)target
+                         action:(SEL)action
+{
+    return [self observerWithConfiguration:^(LRNotificationObserver *observer) {
+        [observer configureForName:name target:target action:action];
+    }];
+}
+
++ (instancetype)observerForName:(NSString *)name
+                 operationQueue:(NSOperationQueue *)operationQueue
+                         target:(id)target
+                         action:(SEL)action
+{
+    return [self observerWithConfiguration:^(LRNotificationObserver *observer) {
+        [observer configureForName:name operationQueue:operationQueue target:target action:action];
+    }];
+}
+
++ (instancetype)observerForName:(NSString *)name
+                  dispatchQueue:(dispatch_queue_t)dispatchQueue
+                         target:(id)target
+                         action:(SEL)action
+{
+    return [self observerWithConfiguration:^(LRNotificationObserver *observer) {
+        [observer configureForName:name dispatchQueue:dispatchQueue target:target action:action];
+    }];
+}
+
+#pragma mark - Private
+
++ (LRNotificationObserver *)observerWithConfiguration:(void(^)(LRNotificationObserver *observer))configurationBlock
+{
+    LRNotificationObserver *observer = [self notificationCenterObserver];
+    configurationBlock(observer);
+    return observer;
+}
+
++ (LRNotificationObserver *)notificationCenterObserver
+{
+    NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
+    
+    return [[LRNotificationObserver alloc] initWithNotificationCenter:notificationCenter];
+}
+
+@end
